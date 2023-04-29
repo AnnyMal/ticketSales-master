c4388329a17e89ad94d4d18948b6d8b2ee1acfd8
AnnyMal <89228060160q@gmail.com>
2023-04-29 09:48:45 +0300

    .

---
+++ src/app/pages/setting/statistic/statistic.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {StatisticService} from "../../../services/statistic/statistic.service";
+import {ICustomStatisticUser} from "../../../models/users";
+
+@Component({
+  selector: 'app-statistic',
+  templateUrl: './statistic.component.html',
+  styleUrls: ['./statistic.component.scss']
+})
+export class StatisticComponent implements OnInit {
+  cols = [
+    {field: "name", header: 'Имя'},
+    {field: 'company',header: 'Компания'},
+    {field: 'phone', header: 'Телефон'},
+    {field: 'city', header: 'Город'},
+    {field: 'street', header: 'Улица'}
+  ];
+  users: ICustomStatisticUser[];
+
+  constructor(private staticticService: StatisticService) { }
+
+  ngOnInit(): void {
+    this.staticticService.getUserStatistic().subscribe((data) =>{
+      this.users = data;
+    })
+  }
+
+}
